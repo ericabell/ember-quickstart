763caef40702c44b2da1de05c1f77b2291a86ac0
Eric Abell <ericabell@users.noreply.github.com>
2017-09-17 12:49:18 -0500

    Add model method to scientists route which returns data for our template

--- app/routes/scientists.js
+++ app/routes/scientists.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  // in the model method, we return any data we want to make
+  // available in the template. If there are async calls, the
+  // model() supports any libraries using Promises.
+  model() {
+    return['Marie Curie', 'Mae Jemison', 'Albert Hofmann' ]
+  }
 });
